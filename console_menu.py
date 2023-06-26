be7d6c88e87d9465f38eada44cc80a0bd2fcac7e
Mathis Jankovic <mathis.jankovic@sikiwis.com>
2023-06-26 14:56:40 +0200

    add function & doc & example

--- console_menu.py
+++ console_menu.py
@@ -7,44 +7,63 @@ class Keys:
     ARROW_DOWN = b"P"
     ENTER = b'\r'
 
-OPTIONS = ["Manger", "Dormir", "Sauvegarder", "Quitter"]
+def console_menu(title: str, options: tuple | list, cursor_color: str) -> str :
+    """Creates a console GUI menu with a cursor and returns the selected option. Use arrow keys to move the cursor.
+    Cursor colors available : black, red, green, yellow, blue, magenta, cyan, white, lightblack, lightred, lightgreen, lightyellow, lightblue, lightmagenta, lightcyan, lightwhite"""
 
-TERMINAL_HEIGHT = os.get_terminal_size().lines
-TERMINAL_WIDTH = os.get_terminal_size().columns
+    TERMINAL_HEIGHT = os.get_terminal_size().lines
+    TERMINAL_WIDTH = os.get_terminal_size().columns
 
-VERTICAL_SPACING = (TERMINAL_HEIGHT - len(OPTIONS)) // 2
-cursor_height = VERTICAL_SPACING # cursor initial position
-key = None
+    VERTICAL_SPACING = (TERMINAL_HEIGHT - len(options)) // 2
+    cursor_height = VERTICAL_SPACING
+    key = None
 
-os.system("cls")
-print('\033[?25l', end="") # Hides cursor
+    if("light" in cursor_color):
+        colorama_cursor_color = cursor_color.upper() + "_EX"
+    else:
+        colorama_cursor_color = cursor_color.upper()
 
-while(key != Keys.ENTER):
-    print("\n"*(VERTICAL_SPACING - 3))
-    print("  Menu en console incroyable".center(TERMINAL_WIDTH))
-    print("\n")
+    try:
+        colorama_color = getattr(colorama.Back, colorama_cursor_color)
+    except AttributeError:
+        raise ValueError(f"'{cursor_color}' is not a valid option for cursor color, please check the function doc")
 
-    for i, option in enumerate(OPTIONS):
-        if(i + VERTICAL_SPACING == cursor_height):
-            print(colorama.Back.BLUE + option.center(TERMINAL_WIDTH) + colorama.Back.BLACK)
-        else:
-            print(option.center(TERMINAL_WIDTH))
+    os.system("cls" if os.name == 'nt' else "clear")
+    print('\033[?25l', end="") # Hides cursor
 
-    while(not msvcrt.kbhit()):
-        pass
+    while(key != Keys.ENTER):
+        print("\n"*(VERTICAL_SPACING - 3))
+        print(title.center(TERMINAL_WIDTH))
+        print("\n")
 
-    key = msvcrt.getch()
+        for i, option in enumerate(options):
+            if(i + VERTICAL_SPACING == cursor_height):
+                print(colorama_color + option.center(TERMINAL_WIDTH) + colorama.Back.BLACK)
+            else:
+                print(option.center(TERMINAL_WIDTH))
 
-    if(key == Keys.ARROW_UP):
-        if(cursor_height > VERTICAL_SPACING):
-            cursor_height = cursor_height - 1  # Moves up the cursor
-        else:
-            cursor_height = VERTICAL_SPACING + len(OPTIONS) - 1 # Moves the cursor at the bottom of the list
-    elif(key == Keys.ARROW_DOWN):
-        if(cursor_height < VERTICAL_SPACING + len(OPTIONS) - 1):
-            cursor_height = cursor_height + 1  # Moves down the cursor
-        else:
-            cursor_height = VERTICAL_SPACING # Moves the cursor at the top of the list
+        while(not msvcrt.kbhit()):
+            pass
 
-    print("\033[F" * (len(OPTIONS) + VERTICAL_SPACING + 2), end="")
-print('\033[? 25h', end="")
+        key = msvcrt.getch()
+
+        if(key == Keys.ARROW_UP):
+            if(cursor_height > VERTICAL_SPACING):
+                cursor_height = cursor_height - 1
+            else:
+                cursor_height = VERTICAL_SPACING + len(options) - 1
+        elif(key == Keys.ARROW_DOWN):
+            if(cursor_height < VERTICAL_SPACING + len(options) - 1):
+                cursor_height = cursor_height + 1
+            else:
+                cursor_height = VERTICAL_SPACING
+
+        print("\033[F" * (len(options) + VERTICAL_SPACING + 3), end="")
+    print('\033[? 25h', end="")
+
+    return options[cursor_height - VERTICAL_SPACING]
+
+if(__name__ == "__main__"):
+    OPTIONS = ["Option 1", "Option 2", "Option 3", "Quit"]
+    choice = console_menu("Welcome", OPTIONS, "lightblue")
+    print(choice)

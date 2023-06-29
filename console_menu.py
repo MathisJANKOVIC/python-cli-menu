f603cda9e0c98720dddfe4888753c73d8575edc8
Rasting <116147352+MathisJANKOVIC@users.noreply.github.com>
2023-06-29 17:33:50 +0200

    correct not centered cursor

--- console_menu.py
+++ console_menu.py
@@ -18,7 +18,7 @@ ANSI_BG_COLORS = {
 
 def console_menu(title: str, options: tuple | list, cursor_color: str) -> str :
     """Creates a console GUI menu with a cursor and returns the selected option. Use arrow keys to move the cursor.
-    Cursor colors available : red, green, yellow, blue, magenta, cyan. You can use a custom color by specify ANSI color code using octal escape code '\\033'"""
+    Cursor colors available : red, green, yellow, blue, magenta, cyan. You can use a custom color by specifing ANSI color code using octal escape code '\\033'"""
 
     TERMINAL_HEIGHT = os.get_terminal_size().lines
     TERMINAL_WIDTH = os.get_terminal_size().columns
@@ -27,24 +27,24 @@ def console_menu(title: str, options: tuple | list, cursor_color: str) -> str :
     cursor_height = VERTICAL_SPACING
     key = None
 
-    if(not cursor_color.startswith("\033[")):
+    if(not cursor_color.startswith("\033")):
         cursor_color = ANSI_BG_COLORS.get(cursor_color)
         if(cursor_color == None):
-            raise ValueError(f"'{cursor_color}' is not a valid option for cursor color, please check the function documentation")
+            raise ValueError(f"'{cursor_color}' is not a valid option for cursor color please check the function documentation.")
 
     os.system("cls" if os.name == "nt" else "clear")
     print('\033[?25l', end="") # Hides cursor
 
     while(key != Keys.ENTER):
         print("\n"*(VERTICAL_SPACING - 3))
-        print(title.center(TERMINAL_WIDTH))
+        print("  " + title.center(TERMINAL_WIDTH-2))
         print("\n")
 
         for i, option in enumerate(options):
             if(i + VERTICAL_SPACING == cursor_height):
-                print(cursor_color + option.center(TERMINAL_WIDTH) + "\033[0m")
+                print("  " + cursor_color + option.center(TERMINAL_WIDTH-2) + "\033[0m")
             else:
-                print(option.center(TERMINAL_WIDTH))
+                print("  " + option.center(TERMINAL_WIDTH-2))
 
         while(not msvcrt.kbhit()):
             pass
@@ -69,5 +69,5 @@ def console_menu(title: str, options: tuple | list, cursor_color: str) -> str :
 
 if(__name__ == "__main__"):
     OPTIONS = ["Option 1", "Option 2", "Option 3", "Quit"]
-    choice = console_menu("Welcome", OPTIONS, "green")
+    choice = console_menu("Amazing Console", OPTIONS, "\033[48;5;202m")
     print(choice)

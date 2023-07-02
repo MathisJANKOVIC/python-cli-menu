61133b51c9fdc0af57b3ec3923aaf101daf37a69
unknown <mathis.jankovic@gmail.com>
2023-07-02 23:15:55 +0200

    update readme

---
+++ README.md
@@ -0,0 +1,35 @@
+# Console-GUI-Menu
+
+Console GUI Menu is Python module that allows you to easilly create a graphical user interface menu on console without any external library. You can select an option by moving the cursor using arrow keys.
+
+The module provides a function that allows you to create the menu and custom the title, options and cursor color. The function returns the selected option. Available colors are red, green, yellow, blue, magenta, cyan. You can also use a custom color by specifing ANSI color code using octal escape code starting with '\033'.
+
+<br>![qsd](/screen_menu.png)
+
+## Quickstart
+
+```python
+import console_menu as cm
+
+OPTIONS = ["Option 1", "Option 2", "Option 3", "Quit"]
+
+# Creates a console menu with blue cursor
+choice1 = cm.console_menu(title="Amazing Console Menu", options=OPTIONS, cursor_color="blue")
+
+# Creates a console menu with white cursor
+choice2 = cm.console_menu(title="Amazing Console Menu", options=OPTIONS, cursor_color="\033[47m")
+
+print(choice1) # output : "Option 1"
+```
+
+## Requirements
+- Python
+- Git (optionnal)
+
+## Configuration
+
+To use the librairy you can simply download the file and move it in your project or follow those steps to move the file into your python librairy file :
+
+    1. Download and extract the zip or clone the project
+    2. Select and copy the file console_menu.py
+    3. Navigate and paste the file into your python library directory (usually in "C:\Program Files\Python\Lib")
---
+++ console_menu.py
@@ -0,0 +1,70 @@
+import msvcrt
+import os
+
+class Keys:
+    ARROW_UP = b"H"
+    ARROW_DOWN = b"P"
+    ENTER = b'\r'
+
+ANSI_BG_COLORS = {
+    "red": "\033[41m",
+    "green": "\033[42m",
+    "yellow": "\033[43m",
+    "blue": "\033[44m",
+    "magenta": "\033[45m",
+    "cyan": "\033[46m",
+    "white": "\033[47m"
+}
+
+def console_menu(title: str, options: tuple | list, cursor_color: str) -> str :
+    """Creates a console GUI menu with a cursor and returns the selected option. Use arrow keys to move the cursor.
+    Cursor colors available : red, green, yellow, blue, magenta, cyan. You can use a custom color by specifing ANSI color code using octal escape code '\\033'"""
+
+    TERMINAL_HEIGHT = os.get_terminal_size().lines
+    TERMINAL_WIDTH = os.get_terminal_size().columns
+
+    VERTICAL_SPACING = (TERMINAL_HEIGHT - len(options)) // 2
+    cursor_height = VERTICAL_SPACING
+    key = None
+
+    if(not cursor_color.startswith("\033")):
+        cursor_color = ANSI_BG_COLORS.get(cursor_color)
+        if(cursor_color == None):
+            raise ValueError(f"'{cursor_color}' is not a valid option for cursor color please check the function documentation.")
+
+    os.system("cls" if os.name == "nt" else "clear")
+    print('\033[?25l', end="") # Hides cursor
+
+    while(key != Keys.ENTER):
+        print("\n"*(VERTICAL_SPACING - 3))
+        print("  " + title.center(TERMINAL_WIDTH - 2))
+        print("\n")
+
+        for i, option in enumerate(options):
+            if(i + VERTICAL_SPACING == cursor_height):
+                print("  " + cursor_color + option.center(TERMINAL_WIDTH - 2) + "\033[0m")
+            else:
+                print("  " + option.center(TERMINAL_WIDTH-2))
+
+        key = msvcrt.getch()
+
+        if(key == Keys.ARROW_UP):
+            if(cursor_height > VERTICAL_SPACING):
+                cursor_height = cursor_height - 1
+            else:
+                cursor_height = VERTICAL_SPACING + len(options) - 1
+        elif(key == Keys.ARROW_DOWN):
+            if(cursor_height < VERTICAL_SPACING + len(options) - 1):
+                cursor_height = cursor_height + 1
+            else:
+                cursor_height = VERTICAL_SPACING
+
+        print("\033[F" * (len(options) + VERTICAL_SPACING + 3), end="")
+    print('\033[? 25h', end="")
+
+    return options[cursor_height - VERTICAL_SPACING]
+
+if(__name__ == "__main__"):
+    OPTIONS = ["Option 1", "Option 2", "Option 3", "Quit"]
+    choice = console_menu("Amazing Console Menu", OPTIONS, "blue")
+    print(choice)

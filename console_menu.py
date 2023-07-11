2a54a965e5efe00d7a546d3d0200fa9711d12431
Rasting <116147352+MathisJANKOVIC@users.noreply.github.com>
2023-07-11 17:28:55 +0200

    Update doc & print to sys.stdout.write

--- console_menu.py
+++ console_menu.py
@@ -1,52 +1,55 @@
+import sys
 import os
 
 class Keys:
-    ARROW_UP = b"H"
-    ARROW_DOWN = b"P"
+    ARROW_UP = b'H'
+    ARROW_DOWN = b'P'
     ENTER = b'\r'
 
 ANSI_BG_COLORS = {
-    "red": "\033[41m",
-    "green": "\033[42m",
-    "yellow": "\033[43m",
-    "blue": "\033[44m",
-    "magenta": "\033[45m",
-    "cyan": "\033[46m",
-    "white": "\033[47m"
+    "red": '\033[41m',
+    "green": '\033[42m',
+    "yellow": '\033[43m',
+    "blue": '\033[44m',
+    "magenta": '\033[45m',
+    "cyan": '\033[46m',
+    "white": '\033[47m'
 }
 
-def console_menu(title: str | list | tuple, options: list | tuple, cursor_color: str) -> str :
-    """Creates a console GUI menu with a cursor and returns the selected option. Use arrow keys to move the cursor.
-    Cursor colors available : red, green, yellow, blue, magenta, cyan. You can use a custom color by specifing ANSI color code using octal escape code '\\033'"""
+def menu(title: str | list | tuple, options: list | tuple, cursor_color: str) -> str :
+    """Creates a console GUI menu with a cursor and returns the selected option. Pass a list or a tuple to 'title' to print the title on multiple lines. Use arrow keys to move the cursor.
+    Cursor colors available : red, green, yellow, blue, magenta, cyan.
+    You can use a custom color by specifing ANSI color code using octal escape code '\\033'. """
 
     TERMINAL_HEIGHT = os.get_terminal_size().lines
     TERMINAL_WIDTH = os.get_terminal_size().columns
 
     VERTICAL_SPACING = (TERMINAL_HEIGHT - len(options)) // 2
     cursor_height = VERTICAL_SPACING
-    key = None
 
-    if(not cursor_color.startswith("\033")):
+    if(not cursor_color.startswith('\033')):
         if(ANSI_BG_COLORS.get(cursor_color) == None):
             raise ValueError(f"'{cursor_color}' is not a valid option for cursor color please check the function documentation.")
         else:
             cursor_color = ANSI_BG_COLORS.get(cursor_color)
 
-    os.system("cls" if os.name == "nt" else "clear")
-    print('\033[?25l', end="") # Hides cursor
+    os.system("cls" if os.name == 'nt' else "clear")
+    sys.stdout.write('\033[?25l') # Hides cursor
 
     if(type(title) == str):
         title = [title]
 
+    key: bytes = None
     while(key != Keys.ENTER):
-        print("\n"*(VERTICAL_SPACING - len(title) - 2))
+
+        print('\n'*(VERTICAL_SPACING - len(title) - 2))
         for line in title:
             print(" " + line.center(TERMINAL_WIDTH - 1))
-        print("\n")
+        print('\n')
 
         for line, option in enumerate(options):
             if(line + VERTICAL_SPACING == cursor_height):
-                print(" " + cursor_color + option.center(TERMINAL_WIDTH - 1) + "\033[0m")
+                print(" " + cursor_color + option.center(TERMINAL_WIDTH - 1) + '\033[0m')
             else:
                 print(" " + option.center(TERMINAL_WIDTH - 1))
 
@@ -68,14 +71,14 @@ def console_menu(title: str | list | tuple, options: list | tuple, cursor_color:
             else:
                 cursor_height = VERTICAL_SPACING
 
-        print("\033[F" * (VERTICAL_SPACING + len(option) + len(title) + 2), end="")
+        sys.stdout.write('\033[F' * (VERTICAL_SPACING + len(option) + len(title) + 2))
 
-    print('\033[?25h', end="") # show cursor
+    sys.stdout.write('\033[?25h') # show cursor
     os.system("cls" if os.name == "nt" else "clear")
 
     return options[cursor_height - VERTICAL_SPACING]
 
 if(__name__ == "__main__"):
     OPTIONS = ["Option 1", "Option 2", "Option 3", "Quit"]
-    choice = console_menu("Amazing Console Menu", OPTIONS, "red")
+    choice = menu("Amazing Console Menu", OPTIONS, "red")
     print(choice)

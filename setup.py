9d663edf09606f8ad2e2fdea5d626cc1c76dc20d
unknown <mathis.jankovic@gmail.com>
2023-10-24 00:59:30 +0200

    changed project name

--- setup.py
+++ setup.py
@@ -1,18 +1,17 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="py-console-menu",
+    name="py-cli-menu",
     version='1.0',
     author="Rasting (Mathis Jankovic)",
     author_email="mathis.jankovic@gmail.com",
-    description="A cross-platform module to easilly create pretty menu in console",
-    packages=find_packages(),
+    description="A simple cross-platform module to create pretty menu in console",    packages=find_packages(),
     keywords=['python', 'menu', 'console'],
     url="https://github.com/MathisJANKOVIC/py-console-menu",
     classifiers=[
         "License :: OSI Approved :: MIT License",
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.10+',
+        'Programming Language :: Python :: 3.10',
         'Operating System :: OS Independent',
         "Intended Audience :: Developers"
     ]

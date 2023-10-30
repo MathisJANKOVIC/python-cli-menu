0039db651ed6abbf963eea995eb9d95048982d23
unknown <mathis.jankovic@gmail.com>
2023-10-31 00:50:15 +0100

    changed exception message

--- pyclimenu/__init__.py
+++ pyclimenu/__init__.py
@@ -5,5 +5,5 @@ __python_version = platform.python_version_tuple()
 
 if(int(__python_version[0]) != 3 or int(__python_version[1]) < 10):
     raise RuntimeError(
-        "Python 3.10 or higher is required"
+        "Python 3.10 or a newer version is required"
     )

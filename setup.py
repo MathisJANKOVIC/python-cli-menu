1460781856c7dc5d6e559debaa5819b0bf52c87d
MathisJANKOVIC <mathis.jankovic@gmail.com>
2024-01-17 19:03:55 +0100

    Add doc for PyPi

--- setup.py
+++ setup.py
@@ -1,16 +1,24 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as readme_file:
+    readme_content = readme_file.readlines()
+
+del readme_content[14:20] # remove installation part
+long_description = "".join(readme_content)
+
 setup(
-    name="python-cli-menu",
-    version='1.1',
-    license="MIT",
-    author="Rasting (Mathis Jankovic)",
-    author_email="mathis.jankovic@gmail.com",
-    description="A simple cross-platform module to create pretty menu in console",
-    packages=find_packages(),
-    keywords=["python", "menu", "console", "cli", "command", "line"],
-    url="https://github.com/MathisJANKOVIC/py-cli-menu",
-    classifiers=[
+    name = "python-cli-menu",
+    version = '1.1',
+    license = "MIT",
+    author = "Rasting (Mathis Jankovic)",
+    author_email = "mathis.jankovic@gmail.com",
+    description = "A simple cross-platform module to create pretty menu in console",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    packages = find_packages(),
+    keywords = ["python", "menu", "console", "cli", "command", "line"],
+    url = "https://github.com/MathisJANKOVIC/py-cli-menu",
+    classifiers = [
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.10",
